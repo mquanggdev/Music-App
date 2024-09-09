6d219d3cf41df30c3d003d9c21db6c572cf1c276
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-09-09 22:04:20 +0700

    12 : Tính dang yêu thích bài hát

--- routes/client/song.route.ts
+++ routes/client/song.route.ts
@@ -6,5 +6,5 @@ import * as controller from "../../controllers/client/song.controller";
 router.get("/:slugTopic", controller.list);
 router.get("/detail/:slugSong" , controller.detail);
 router.patch("/like/:status/:songId", controller.like);
-
+router.patch("/favorite/:status/:songId", controller.favorite);
 export const songRoutes: Router = router;
