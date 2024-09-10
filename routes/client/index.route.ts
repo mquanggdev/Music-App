cdd85a47104e62956e1e3555891035c6dde57eb3
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-09-10 10:21:02 +0700

    14 : Tính năng tìm kiếm bài hát cơ bản

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -2,11 +2,13 @@ import { Express } from "express";
 import { topicRoutes } from "./topic.route";
 import { songRoutes } from "./song.route";
 import { favoriteRoutes } from "./favorite-song.route";
+import { searchRoutes } from "./search.route";
 const clientRoutes = (app: Express): void => {
 
   app.use(`/topics`, topicRoutes);
   app.use(`/songs`, songRoutes);
   app.use(`/favorite-songs`, favoriteRoutes);
+  app.use(`/search`, searchRoutes);
 };
 
 export default clientRoutes;
