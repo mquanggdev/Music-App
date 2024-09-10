2d754ad979e363ccf212da91543f493642525963
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-09-10 10:42:53 +0700

    15 : Tính năng tìm kiếm nâng cao

--- controllers/client/search.controller.ts
+++ controllers/client/search.controller.ts
@@ -1,27 +1,39 @@
 import { Request , Response } from "express";
 import Song from "../../models/song.model";
 import Singer from "../../models/singer.model";
+import unidecode from "unidecode";
 
 //GET /topic
 export const result = async (req : Request , res : Response) => {
     const keyword: string = `${req.query.keyword}`;
 
-  const keyWordRegex = new RegExp(keyword, "i");
+    const unidecodeText: string = unidecode(keyword.trim());
+    const keywordSlug = unidecodeText.replace(/\s+/g, "-");
+    const keyWordSlugRegex = new RegExp(keywordSlug, "i");
 
-  const songs = await Song.find({
-    title: keyWordRegex,
-    deleted: false,
-    status: "active"
-  }).select("avatar title singerId like slug");
-
-  for (const item of songs) {
-    const singer = await Singer.findOne({
-      _id: item.singerId,
-      deleted: false
-    }).select("fullName");
-
-    item["singer"] = singer;
-  }
+    const keyWordRegex = new RegExp(keyword, "i");
+    let songs = [];
+    if(keyword){
+       songs = await Song.find({
+        $or : [
+          { slug: keyWordSlugRegex },
+          { title: keyWordRegex }
+        ],
+        deleted: false,
+        status: "active"
+      }).select("avatar title singerId like slug");
+    
+      for (const item of songs) {
+        const singer = await Singer.findOne({
+          _id: item.singerId,
+          deleted: false
+        }).select("fullName");
+    
+        item["singer"] = singer;
+      }
+    }
+    
+  
 
   res.render("client/pages/search/result", {
     pageTitle: `Kết quả: ${keyword}`,
