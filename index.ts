3e47eb6f746b4b00299ce14c1f12691e32c3ca11
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-09-12 10:08:38 +0700

    19 : Layout chung bên admin

--- index.ts
+++ index.ts
@@ -5,6 +5,9 @@ env.config();
 import { connect } from "./config/database";
 connect();
 import clientRoutes from "./routes/client/index.route";
+import adminRoutes from "./routes/admin/index.route";
+import { systemConfig } from "./config/system";
+import path from "path";
 
 const port:number|string = `${process.env.PORT}` || 3000  ;
 
@@ -12,8 +15,17 @@ const port:number|string = `${process.env.PORT}` || 3000  ;
 app.set('views' , `${__dirname}/views`);
 app.set('view engine' , 'pug');
 app.use(express.static(`${__dirname}/public`));
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+// TinyMCE
+app.use(
+  "/tinymce",
+  express.static(path.join(__dirname, "node_modules", "tinymce"))
+);
+// End TinyMCE
+
 
 clientRoutes(app);
+adminRoutes(app);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
