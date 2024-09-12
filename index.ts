e22aefb1d1d8c0e38c86c7a1bc32757b5183ae93
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-09-12 16:37:08 +0700

    22 : Thêm mới 1 bài hát

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ import clientRoutes from "./routes/client/index.route";
 import adminRoutes from "./routes/admin/index.route";
 import { systemConfig } from "./config/system";
 import path from "path";
+import bodyParser from "body-parser";
 
 const port:number|string = `${process.env.PORT}` || 3000  ;
 
@@ -16,6 +17,8 @@ app.set('views' , `${__dirname}/views`);
 app.set('view engine' , 'pug');
 app.use(express.static(`${__dirname}/public`));
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 // TinyMCE
 app.use(
   "/tinymce",
